c20ef6052d4a12fd1b7fc86bc5a8b938265f463f
cikho98 <1165667817@qq.com>
2021-09-06 14:36:36 +0800

    数组之和

--- LeetCode/NumSum.java
+++ LeetCode/NumSum.java
@@ -2,11 +2,11 @@ package Javatest.LeetCode;
 
 public class NumSum {
     public static void main(String[] args) {
-        int[] nums ={0,1,2,3,4,5,2,3,3,1};
+        int[] nums ={2,1,2,3,4,5,2,3,3,1};
         int targets=4;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length-i-1;j++){
-                if (nums[i] == nums[j]){
+                if (i==j){
                     continue;
                 } else if (nums[i] + nums[j] == targets){
                     System.out.println(nums[i]+"\t"+nums[j]);
